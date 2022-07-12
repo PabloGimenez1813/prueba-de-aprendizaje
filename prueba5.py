27da84d39a5af024d16eead487feae9a83d9dedc
Pablo Gimenez <maximogiovanne@gmail.com>
2022-07-12 01:02:43 -0300

    nuevo codigo

--- prueba5.py
+++ prueba5.py
@@ -1,3 +1,8 @@
 prueba= 2
 prueba1 = 7
-print("rsultado es: ", prueba+prueba1)
+resultado=prueba+prueba1
+print("el resultado es: ", resultado)
+if prueba >= 5:
+    print ("es mayor que" 5)
+else: print ("es menor que 5")
+print("fin")
